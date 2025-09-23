0ac2bf6812bfd2f67db9fe023111bedc13765487
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-09-23 21:45:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.cpp
@@ -0,0 +1,17 @@
+#include <queue>
+using namespace std;
+
+class Solution {
+  public:
+    void reverseQueue(queue<int> &q) {
+     
+        if (q.empty()) return;
+
+        int x = q.front();
+        q.pop();
+
+        reverseQueue(q);
+
+        q.push(x);
+    }
+};
