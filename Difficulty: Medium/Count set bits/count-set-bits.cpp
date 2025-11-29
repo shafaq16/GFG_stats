e42396f2e0929bc807b37001d582e0d084999909
Shafaqun Nisa <nisashafaqun@gmail.com>
2025-11-29 22:13:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count set bits/count-set-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int countSetBits(int n) {
+        // code here
+        int limit = log2(n);    
+        
+        int ans=0;
+        
+        for (int idx=0; idx<=limit; idx++) {
+            int pattern = (1<<(idx+1));
+            
+            int noOfCycles = (n+1)/pattern; 
+            int noOfOnes = noOfCycles*(pattern/2); 
+            ans += noOfOnes;
+            
+            int remain = (n+1)%pattern;
+            remain -= (pattern/2);   
+            if (remain>0) ans += remain;   
+        }
+        
+        return ans;
+    }
+};
