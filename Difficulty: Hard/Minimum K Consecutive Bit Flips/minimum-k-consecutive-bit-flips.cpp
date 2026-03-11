b4452f9c85b5b612331e50d2e748ff0910dd0ace
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-03-12 00:56:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int kBitFlips(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size();
+        queue<int> q;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            
+           if(!q.empty() && i==q.front()){
+               q.pop();
+           }
+              
+           int bit=arr[i];
+           
+           if(!q.empty() && q.size()%2==1){
+               bit^=1;//if odd size->change the bit
+           }
+           if(bit==0){
+               if(i+k>n)return -1;
+               ans++;
+               q.push(i+k);
+           }
+        }
+        return ans;
+    }
+};
