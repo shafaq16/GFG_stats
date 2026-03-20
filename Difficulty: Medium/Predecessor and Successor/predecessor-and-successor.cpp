bd519c63de3312d9b65cd602379d2a85b22618d0
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-03-20 21:58:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+  void find(Node* root,int key,vector<Node*>&ans){
+      if(root==NULL) return;
+      find(root->left,key,ans);
+      ans.push_back(root);
+      find(root->right,key,ans);
+  }
+    vector<Node*> findPreSuc(Node* root, int key) {
+        // code here
+        vector<Node *>ans;
+        find(root,key,ans);
+        int indexS=-1;
+        int indexG=-1;
+        bool flag=true;
+        for(int i=0;i<ans.size();i++){
+
+            if(ans[i]->data<key){
+                indexS=i;
+            }
+            else if(flag && ans[i]->data>key){
+                indexG=i;
+                flag=false;
+            }
+        }
+        vector<Node*>final(2,nullptr);
+        if(indexS!=-1){
+            final[0]=ans[indexS];
+        }
+        if(indexG!=-1){
+            final[1]=ans[indexG];
+        }
+        return final;
+}
+};
+
