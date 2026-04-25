2a7d22bae81f57243df23559c196390a2e38b357
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-04-26 01:28:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+        int i = 0, j = 0, k = 0;
+        vector<int> ans;
+
+        while(i < a.size() && j < b.size() && k < c.size()){
+            if(a[i] == b[j] && b[j] == c[k]){
+                if(ans.empty() || ans.back() != a[i]) // avoid duplicates
+                    ans.push_back(a[i]);
+                i++; j++; k++;
+            }
+            else if(a[i] < b[j]){
+                i++;
+            }
+            else if(b[j] < c[k]){
+                j++;
+            }
+            else{
+                k++;
+            }
+        }
+        return ans;
+    }
+};
