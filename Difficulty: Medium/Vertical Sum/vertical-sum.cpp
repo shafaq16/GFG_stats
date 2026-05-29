74e65aebe2226a54ddc5d6d13970b2d1d3b0f309
Shafaqun Nisa <nisashafaqun@gmail.com>
2026-05-29 18:38:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Vertical Sum/vertical-sum.cpp
+++ Difficulty: Medium/Vertical Sum/vertical-sum.cpp
@@ -11,36 +11,57 @@ public:
     }
 }
 */
-class Solution {
-  public:
-    vector<int> verticalSum(Node* root) {
-        // code here
-        map<int, map<int , multiset<int>>> mp;
-        queue<pair<Node*, pair<int,int>>> q;
-        q.push({root, {0,0}});
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            Node* node = p.first;
-            int x = p.second.first, y = p.second.second ;
-            mp[x][y].insert(node->data);
-            if(node->left){
-                q.push({node->left, {x-1, y+1}});
-            }
-            if(node->right){
-                q.push({node->right, {x+1, y+1}});
-            }
-        }
+// class Solution {
+//   public:
+//     vector<int> verticalSum(Node* root) {
+//         // code here
+//         map<int, map<int , multiset<int>>> mp;
+//         queue<pair<Node*, pair<int,int>>> q;
+//         q.push({root, {0,0}});
+//         while(!q.empty()){
+//             auto p = q.front();
+//             q.pop();
+//             Node* node = p.first;
+//             int x = p.second.first, y = p.second.second ;
+//             mp[x][y].insert(node->data);
+//             if(node->left){
+//                 q.push({node->left, {x-1, y+1}});
+//             }
+//             if(node->right){
+//                 q.push({node->right, {x+1, y+1}});
+//             }
+//         }
         
+//         vector<int> ans;
+//         for(auto x : mp){
+//         int sum = 0;
+//             for(auto y : x.second){
+//                 for(auto val : y.second){
+//                     sum += val;
+//                 }
+//             }
+//             ans.push_back(sum);
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+
+    void preorder(Node* root, int x, map<int,int>& mp){
+        if(root == NULL) return;
+        mp[x] += root->data;
+        preorder(root->left, x - 1, mp);
+        preorder(root->right, x + 1, mp);
+    }
+
+    vector<int> verticalSum(Node *root) {
+        map<int,int> mp;
+        preorder(root, 0, mp);
         vector<int> ans;
-        for(auto x : mp){
-        int sum = 0;
-            for(auto y : x.second){
-                for(auto val : y.second){
-                    sum += val;
-                }
-            }
-            ans.push_back(sum);
+        for(auto pair : mp){
+            ans.push_back(pair.second);
         }
         return ans;
     }
